c404003bfc969a168f6407f63a4120cd99e5d7bb
Daniel <64984840+acegxntil@users.noreply.github.com>
2022-09-02 19:26:15 -0300

    Add files via upload

---
+++ main.f95
@@ -0,0 +1,17 @@
+Program mediadedoisnumeros
+
+real*4 nota1, nota2, soma, media
+
+print*, "Digite a primeira nota:"
+read(*,*) nota1
+
+print*, "Digite a segunda nota:"
+read (*,*) nota2
+
+soma = nota1+nota2
+media = soma/2.0
+
+print*, "A média das duas notas é:"
+print*, media
+
+end Program
